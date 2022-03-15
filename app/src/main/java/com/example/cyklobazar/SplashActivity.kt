78b735f6160e6d71c3c719d5718601da8e95918b
Gensor <Gensor@users.noreply.github.com>
2022-03-15 23:47:32 +0100

    Splash Screen

--- app/src/main/java/com/example/cyklobazar/SplashActivity.kt
+++ app/src/main/java/com/example/cyklobazar/SplashActivity.kt
@@ -1,11 +1,22 @@
 package com.example.cyklobazar
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.TextView
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val typeFace : Typeface = Typeface.createFromAsset(assets,"league-gothic.regular.ttf")
+        val tv_app_name = findViewById<TextView>(R.id.tv_app_name)
+        tv_app_name.typeface = typeFace
     }
 }
