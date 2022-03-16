ac39ba0062783485b978f7f5f42fcf8fbda1f5dc
Gensor <Gensor@users.noreply.github.com>
2022-03-16 11:48:18 +0100

    Splash Screen to MainActivity

--- app/src/main/java/com/example/cyklobazar/SplashActivity.kt
+++ app/src/main/java/com/example/cyklobazar/SplashActivity.kt
@@ -1,8 +1,11 @@
 package com.example.cyklobazar
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowManager
 import android.widget.TextView
 
@@ -10,13 +13,14 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
 
         val typeFace : Typeface = Typeface.createFromAsset(assets,"league-gothic.regular.ttf")
         val tv_app_name = findViewById<TextView>(R.id.tv_app_name)
         tv_app_name.typeface = typeFace
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
