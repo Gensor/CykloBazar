48d940c749ab7da4ba04cfcdf7d9df5bf2257729
Gensor <Gensor@users.noreply.github.com>
2022-03-20 23:23:00 +0100

    activity_sign_up.xml v0.2 - iba aby to fungovalo presmerovanie z mainActivity do SignUpActivity toolBar v signUpActivity

--- app/src/main/java/com/example/cyklobazar/MainActivity.kt
+++ app/src/main/java/com/example/cyklobazar/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.cyklobazar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById<Button>(R.id.button_main_login)
+        loginButton.setOnClickListener{
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
