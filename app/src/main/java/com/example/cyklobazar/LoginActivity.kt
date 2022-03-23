f80376e2ad24e84c39962dd3daf7cfc0fbcb7386
Gensor <Gensor@users.noreply.github.com>
2022-03-23 11:27:51 +0100

    pridal activity_login.xml , LoginActivity.kt

---
+++ app/src/main/java/com/example/cyklobazar/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.example.cyklobazar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setupActionBar()
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(toolbar_login_top)
+        val actionBar = supportActionBar
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(com.google.android.material.R.drawable.abc_ic_ab_back_material)
+            actionBar.setDisplayShowTitleEnabled(false)
+        }
+        toolbar_login_top?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
--- app/src/main/java/com/example/cyklobazar/MainActivity.kt
+++ app/src/main/java/com/example/cyklobazar/MainActivity.kt
@@ -10,9 +10,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val signupButton = findViewById<Button>(R.id.button_main_signup)
+        signupButton.setOnClickListener{
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
         val loginButton = findViewById<Button>(R.id.button_main_login)
         loginButton.setOnClickListener{
-            startActivity(Intent(this, SignUpActivity::class.java))
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
